e59ab3dab832248b428707a6013877195417fd63
Cam Peele <christopher.peele21@gmail.com>
2022-03-18 17:23:57 -0400

    Initial commit

---
+++ aframe-test/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import './js/godray_effect';
+
+function App() {
+  return (<div></div>
+    // <a-scene>
+    //   <a-assets>
+    //     <a-asset-item id="house" src="models/interview_test_1.glb"></a-asset-item>
+    //   </a-assets>
+
+    //   <a-gltf-model src="#house"></a-gltf-model>
+    //   {/* <a-light type="spot" look-at="#house" light="color: #fefae1; castShadow: true" position="0 35 48" rotation="-30 0 0"></a-light> */}
+    //   {/* <a-sky src="img/skybox.jpg"></a-sky> */}
+      
+
+    // </a-scene>
+  );
+}
+
+export default App;
